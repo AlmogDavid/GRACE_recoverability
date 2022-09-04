e632f99bf520bd7e7f503dfecca0afd4d1928a37
almogdavid <almogdavid@gmail.com>
2022-09-04 07:23:09 +0300

    sync commit

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,23 +1,4 @@
-wandb agent almogdavid/GRACE_recoverability_hps/im8sp1e8 &
-sleep 30s
-wandb agent almogdavid/GRACE_recoverability_hps/im8sp1e8 &
-sleep 30s
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/2g9g5q8v &
-#wandb agent almogdavid/GRACE_recoverability_hps/1wcmxots &
-#sleep 30s
-#wandb agent almogdavid/GRACE_recoverability_hps/1wcmxots &
+wandb agent almogdavid/GRACE_recoverability_hps/qez0gpv2 &
+sleep 90s
+wandb agent almogdavid/GRACE_recoverability_hps/qez0gpv2 &
+sleep 90s
